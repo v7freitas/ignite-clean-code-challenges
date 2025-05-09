cc63e0cf42c0bd67a978525a0f12fa8fb24c65ed
Vinicius Freitas <vff2804@gmail.com>
2025-05-08 21:45:12 -0300

    renomeia variaveis booleanas

--- desafios/02-booleanos.ts
+++ desafios/02-booleanos.ts
@@ -1,31 +1,31 @@
 // Boleanos
 
 const user = {
-  name: 'Diego Fernandes',
+  name: "Diego Fernandes",
   height: 190,
   hasTicket: true,
+};
+
+const necessaryHeight = 130;
+
+const currentHour = new Date().getHours();
+
+const isParkOpened = currentHour > 9 && currentHour < 18;
+
+if (!isParkOpened) {
+  throw new Error("O parque está fechado!");
 }
 
-const necessaryHeight = 130
+const isTicketAvailable = user.hasTicket;
 
-const currentHour = new Date().getHours()
-
-const parkOpen = currentHour > 9 && currentHour < 18
-
-if (!parkOpen) {
-  throw new Error('O parque está fechado!')
+if (!isTicketAvailable) {
+  throw new Error("O Diego não possui um bilhete para entrar no parque!");
 }
 
-const ticket = user.hasTicket
+const canEnterToy = user.height > necessaryHeight;
 
-if (!ticket) {
-  throw new Error('O Diego não possui um bilhete para entrar no parque!')
+if (!canEnterToy) {
+  throw new Error("O Diego não pode entrar no brinquedo!");
 }
 
-const enterToy = user.height > necessaryHeight
-
-if (!enterToy) {
-  throw new Error('O Diego não pode entrar no brinquedo!')
-} 
-
-console.log('O Diego se divertiu muito! :)')
+console.log("O Diego se divertiu muito! :)");
