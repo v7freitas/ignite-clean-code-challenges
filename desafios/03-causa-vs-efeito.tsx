fefc25407b2bd2342797b1947e0231a4603495fb
Vinicius Freitas <vff2804@gmail.com>
2025-05-08 22:07:24 -0300

    renomeia causa e efeito

--- desafios/03-causa-vs-efeito.tsx
+++ desafios/03-causa-vs-efeito.tsx
@@ -1,5 +1,5 @@
 // Causa vs Efeito
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
 interface User {
   name: string;
@@ -10,33 +10,33 @@ function fetchUser() {
   return {
     data: {
       user: {
-        name: 'Joseph Oliveira',
-        github: 'https://github.com/josepholiveira'
-      }
-    }
-  }
+        name: "Joseph Oliveira",
+        github: "https://github.com/josepholiveira",
+      },
+    },
+  };
 }
 
 export function UserProfile() {
-  const [shouldNotRenderUserName, setShouldNotRenderUserName] = useState(false)
-  const [userData, setUserData] = useState<User>()
+  const [showUserData, setShowUserData] = useState(false);
+  const [userData, setUserData] = useState<User>();
 
   useEffect(() => {
     function loadUser() {
-      setShouldNotRenderUserName(true)
+      setShowUserData(true);
 
-      const fetchUserResponse = fetchUser()
+      const fetchUserResponse = fetchUser();
 
-      setUserData(fetchUserResponse.data.user)
-      
-      setShouldNotRenderUserName(false)
+      setUserData(fetchUserResponse.data.user);
+
+      setShowUserData(false);
     }
 
-    loadUser()
-  })
+    loadUser();
+  });
 
-  if (shouldNotRenderUserName) {
-    return <p>Loading...</p>
+  if (showUserData) {
+    return <p>Loading...</p>;
   }
 
   return (
@@ -44,7 +44,5 @@ export function UserProfile() {
       <img src={`${userData?.github}.png`} alt="" />
       <a href={userData?.github}>{userData?.name}</a>
     </div>
-  )
+  );
 }
-
-
